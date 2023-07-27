0d1be6c95ba5fdd14b353b7e288ae4ed9540c955
Sagar Aryal <sagararyal1122@gmail.com>
2023-07-27 19:52:05 +0545

    Fetch Single Image

--- lib/feature/home/domain/banner_repository.dart
+++ lib/feature/home/domain/banner_repository.dart
@@ -2,5 +2,6 @@ import 'package:go_fresha/core/data/data_source/remote/api_result.dart';
 
 abstract class BannerRepository {
   Future<ApiResponse> fetchSliderImageList();
+  Future<ApiResponse> fetchimage();
   
 }
