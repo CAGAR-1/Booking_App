7a2375237c8f895b854a82403187b926e2911520
Sagar Aryal <sagararyal1122@gmail.com>
2023-08-21 16:03:34 +0545

    fixed banner

--- lib/feature/home/domain/banner_repository.dart
+++ lib/feature/home/domain/banner_repository.dart
@@ -2,6 +2,4 @@ import 'package:go_fresha/core/data/data_source/remote/api_result.dart';
 
 abstract class BannerRepository {
   Future<ApiResponse> fetchSliderImageList();
-  Future<ApiResponse> fetchimage();
-  
 }
